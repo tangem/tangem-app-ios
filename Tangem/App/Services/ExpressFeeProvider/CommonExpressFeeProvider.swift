5dbc94749b5f32c627783a6e21f10f4cf01d0788
Sergey Balashov <Balashov.152@gmail.com>
2024-03-18 17:34:40 +0000

    IOS-5984 Update CommonExpressFeeProvider.swift

--- Tangem/App/Services/ExpressFeeProvider/CommonExpressFeeProvider.swift
+++ Tangem/App/Services/ExpressFeeProvider/CommonExpressFeeProvider.swift
@@ -38,7 +38,8 @@ extension CommonExpressFeeProvider: ExpressFeeProvider {
 
             let gasLimit = parameters.gasLimit * BigUInt(112) / BigUInt(100)
             let feeValue = gasLimit * parameters.gasPrice
-            let fee = feeValue.decimal ?? Decimal(Int(feeValue)) / wallet.tokenItem.blockchain.decimalValue
+            let feeWEI = feeValue.decimal ?? Decimal(Int(feeValue))
+            let fee = feeWEI / wallet.tokenItem.blockchain.decimalValue
             let amount = Amount(with: wallet.tokenItem.blockchain, value: fee)
             return Fee(amount, parameters: EthereumFeeParameters(gasLimit: gasLimit, gasPrice: parameters.gasPrice))
         }
