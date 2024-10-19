f8e8b8596754e73df7d65e888236577c70dd7207
Sergei Balashov <Balashov.152@gmail.com>
2024-10-19 14:25:00 +0100

    IOS-8156 Add OnrampDTOs
    IOS-8156 Update Package.resolved
    
    Update Package.resolved

--- TangemExpress/Models/Onramp/OnrampCurrency.swift
+++ TangemExpress/Models/Onramp/OnrampCurrency.swift
@@ -1,12 +1,12 @@
 //
-//  OnrampCurrency.swift
+//  OnrampFiatCurrency.swift
 //  TangemApp
 //
 //  Created by Sergey Balashov on 02.10.2024.
 //  Copyright © 2024 Tangem AG. All rights reserved.
 //
 
-public struct OnrampCurrency: Hashable {
+public struct OnrampFiatCurrency: Hashable {
     public let identity: OnrampIdentity
 
     public init(identity: OnrampIdentity) {
--- TangemExpress/Models/Onramp/OnrampRedirectData.swift
+++ TangemExpress/Models/Onramp/OnrampRedirectData.swift
@@ -6,6 +6,21 @@
 //  Copyright © 2024 Tangem AG. All rights reserved.
 //
 
-public struct OnrampRedirectData: Hashable {
-    let successURL: URL
+public struct OnrampRedirectData: Hashable, Decodable {
+    let fromCurrencyCode: String
+    let toContractAddress: String
+    let toNetwork: String
+    let paymentMethod: String
+    let countryCode: String
+    let fromAmount: String
+    let toAmount: String?
+    let toDecimals: Int
+    let providerId: String
+    let toAddress: String
+    let redirectUrl: String
+    let language: String?
+    let theme: String?
+    let requestId: String
+    let externalTxId: String
+    let widgetUrl: String
 }
